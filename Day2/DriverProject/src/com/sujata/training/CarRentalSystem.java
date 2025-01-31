30c7f39be5b61140ed30e6987a7a3b87d1a225d3
Sujatabatra <sujatabatra@hotmail.com>
2025-01-31 16:06:09 +0530

    Add files via upload

---
+++ Day2/DriverProject/src/com/sujata/training/CarRentalSystem.java
@@ -0,0 +1,24 @@
+package com.sujata.training;
+
+import com.sujata.demo.Audi;
+import com.sujata.demo.BMW;
+import com.sujata.demo.Scoda;
+
+public class CarRentalSystem {
+
+	public static void main(String[] args) {
+		BMW bmw=new BMW("Blue", "Q6");
+		Audi audi=new Audi("Red", "A8");
+		Scoda scoda=new Scoda("Silver", "Slavia");
+		
+		Driver sonu=new Driver("Sonu");
+		Driver ramesh=new Driver("Ramesh");
+		Driver suresh=new Driver("Suresh");
+		Driver monu=new Driver("Monu");
+		
+		suresh.setCar(scoda);
+		suresh.drive();
+
+	}
+
+}
